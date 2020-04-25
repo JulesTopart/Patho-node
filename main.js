321212b1712cfd644558c6545254328ebe3c64a1
thomas.duvinage <thomas.duvinage@utbm.fr>
2020-04-25 17:17:54 +0200

    update request search

--- main.js
+++ main.js
@@ -277,7 +277,7 @@ app.get('/search', cors(corsOptions), function (req, res) {
 function sqlSearch(keywords, callback) {
     console.log("keywords :", keywords);
 
-    var query = "SELECT `Num_exam`,`libelle_organe`,`libelle_lesion`,CR`, MATCH(`CR`) AGAINST (" + keywords + " IN NATURAL LANGUAGE MODE) AS SCORE FROM `database` ORDER BY SCORE DESC"
+    var query = "SELECT `Num_exam`,`libelle_organe`,`libelle_lesion`,`CR`, MATCH(`CR`) AGAINST ('" + keywords + "' IN NATURAL LANGUAGE MODE) AS SCORE FROM `database` ORDER BY SCORE DESC"
     console.log("Seach Query : ", query);
 
     connection.query(query, function (err, rows) {
